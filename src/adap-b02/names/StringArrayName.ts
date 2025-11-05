c13ece68c0f491089f5499baca25482445a73079
Thomas Maier <167201207+tmaier473@users.noreply.github.com>
2025-11-05 18:10:47 +0000

    Init for b02, refactoring is needed. All tests passed.

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -2,56 +2,161 @@ import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 
 export class StringArrayName implements Name {
+  protected delimiter: string = DEFAULT_DELIMITER;
+  protected components: string[] = [];
 
-    protected delimiter: string = DEFAULT_DELIMITER;
-    protected components: string[] = [];
+  constructor(source: string[], delimiter?: string) {
+    this.initialize(source, delimiter);
+  }
 
-    constructor(source: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+  private initialize(source: string[], delimiter?: string): void {
+    if (delimiter && delimiter.length < 2) {
+      this.setDelimiter(delimiter);
     }
 
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+    this.setComponents(source);
+  }
+
+  public getDelimiterCharacter(): string {
+    return this.doGetDelimiterCharacter();
+  }
+
+  private doGetDelimiterCharacter(): string {
+    return this.delimiter;
+  }
+
+  private setDelimiter(delimiter: string): void {
+    this.doSetDelimiter(delimiter);
+  }
+
+  private doSetDelimiter(delimiter: string): void {
+    this.delimiter = delimiter;
+  }
+
+    private getComponents(): string[] {
+    return this.doGetComponents();
+  }
+
+  private doGetComponents(): string[] {
+    return this.components;
+  }
+
+  private setComponents(components: string[]): void {
+    this.doSetComponents(components);
+  }
+
+  private doSetComponents(components: string[]): void {
+    this.components = components;
+  }
+
+  public asString(delimiter: string = this.delimiter): string {
+    const components: string[] = this.getComponents();
+    const componentsSeperatedByDelimiter: string =
+      components.join(delimiter);
+
+    return componentsSeperatedByDelimiter;
+  }
+
+  public asDataString(): string {
+    const components: string[] = this.getComponents();
+    const maskedComponents: string[] = components.map((component) =>
+      this.asMaskedComponent(component)
+    );
+    const maskedComponentsSeperatedByDelimiter: string =
+      maskedComponents.join(this.getDelimiterCharacter());
+
+    return maskedComponentsSeperatedByDelimiter;
+  }
+
+  /** Escapes all delimiters after escaping the escape character */
+  private asMaskedComponent(c: string): string {
+    let maskedComponent: string = c.replaceAll(
+      ESCAPE_CHARACTER,
+      `${ESCAPE_CHARACTER}${ESCAPE_CHARACTER}`
+    );
+
+    maskedComponent = maskedComponent.replaceAll(
+      this.getDelimiterCharacter(),
+      `${ESCAPE_CHARACTER}${this.getDelimiterCharacter()}`
+    );
+
+    return maskedComponent;
+  }
+
+  public isEmpty(): boolean {
+    return this.getNoComponents() === 0;
+  }
+
+  public getNoComponents(): number {
+    const components: string[] = this.getComponents();
+    return components.length;
+  }
+
+  public getComponent(i: number): string {
+    if (!this.isValidIndexForComponentsArray(i)) {
+      console.error(`${i} is not a valid index for the components array!`);
+      return "";
     }
 
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+    const components: string[] = this.getComponents();
+    return components[i];
+  }
+
+  public setComponent(i: number, c: string): void {
+    if (!this.isValidIndexForComponentsArray(i)) {
+      console.error(`${i} is not a valid index for the components array!`);
+      return;
     }
 
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+    const components: string[] = this.getComponents();
+    components[i] = c
+    this.setComponents(components);
+  }
+
+  public insert(i: number, c: string): void {
+    const components: string[] = this.getComponents();
+    const isValidInsertIndex: boolean =
+      Number.isInteger(i) && i >= 0 && i <= components.length;
+    if (!isValidInsertIndex) {
+      console.error(
+        `${i} is not a valid *insertion* index for the components array!`
+      );
+      return;
     }
 
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+    components.splice(i, 0, c);
+    this.setComponents(components);
+  }
+
+  public append(c: string): void {
+    const components: string[] = this.getComponents();
+    components.push(c);
+    this.setComponents(components);
+  }
+
+  public remove(i: number): void {
+    if (!this.isValidIndexForComponentsArray(i)) {
+      console.error(`${i} is not a valid index for the components array!`);
+      return;
     }
 
-    public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
-    }
+    const components: string[] = this.getComponents();
+    components.splice(i, 1);
+    this.setComponents(components);
+  }
 
-    public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
-    }
+  public concat(other: Name): void {
+    const delimiter: string = this.getDelimiterCharacter();
+    const concatAsString =
+      this.asString() + delimiter + other.asString();
+    this.setComponents(concatAsString.split(delimiter));
+  }
 
-    public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
-    }
+  private isValidIndexForComponentsArray(i: number): boolean {
+    const components: string[] = this.getComponents();
+    const isInteger: boolean = Number.isInteger(i);
+    const isInBounds: boolean = i >= 0 && i < components.length;
 
-    public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public append(c: string): void {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
-    }
-
+    return isInteger && isInBounds;
+  }
 }
--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -2,57 +2,183 @@ import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 
 export class StringName implements Name {
+  protected delimiter: string = DEFAULT_DELIMITER;
+  protected name: string = "";
+  protected noComponents: number = 0;
 
-    protected delimiter: string = DEFAULT_DELIMITER;
-    protected name: string = "";
-    protected noComponents: number = 0;
+  constructor(source: string, delimiter?: string) {
+    this.initialize(source, delimiter);
+  }
 
-    constructor(source: string, delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+  private initialize(source: string, delimiter?: string): void {
+    if (delimiter && delimiter.length < 2) {
+      this.setDelimiter(delimiter);
     }
 
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
+    this.setName(source);
+    const components: string[] = this.getComponents();
+    this.setNoComponents(components.length)
+  }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+    return this.doGetDelimiterCharacter();
+  }
+
+  private doGetDelimiterCharacter(): string {
+    return this.delimiter;
+  }
+
+  private setDelimiter(delimiter: string): void {
+    this.doSetDelimiter(delimiter);
+  }
+
+  private doSetDelimiter(delimiter: string): void {
+    this.delimiter = delimiter;
+  }
+
+  private getName(): string {
+    return this.doGetName();
+  }
+
+  private doGetName(): string {
+    return this.name;
+  }
+
+  private setName(name: string): void {
+    this.doSetName(name);
+  }
+
+  private doSetName(name: string): void {
+    this.name = name;
+  }
+
+  public getNoComponents(): number {
+    return this.doGetNoComponents();
+  }
+
+  private doGetNoComponents(): number {
+    return this.noComponents;
+  }
+
+  public setNoComponents(no: number): void {
+    this.doSetNoComponents(no);
+  }
+
+  private doSetNoComponents(no: number): void {
+    this.noComponents = no;
+  }
+
+  private getComponents(): string[] {
+    const name: string = this.getName();
+    return name.split(this.getDelimiterCharacter());
+  }
+
+  public asString(delimiter: string = this.delimiter): string {
+    const components: string[] = this.getComponents();
+    const componentsSeperatedByDelimiter: string =
+      components.join(delimiter);
+
+    return componentsSeperatedByDelimiter;
+  }
+
+  public asDataString(): string {
+    const components: string[] = this.getComponents();
+    const maskedComponents: string[] = components.map((component) =>
+      this.asMaskedComponent(component)
+    );
+    const maskedComponentsSeperatedByDelimiter: string =
+      maskedComponents.join(this.getDelimiterCharacter());
+
+    return maskedComponentsSeperatedByDelimiter;
+  }
+
+  /** Escapes all delimiters after escaping the escape character */
+  private asMaskedComponent(c: string): string {
+    let maskedComponent: string = c.replaceAll(
+      ESCAPE_CHARACTER,
+      `${ESCAPE_CHARACTER}${ESCAPE_CHARACTER}`
+    );
+
+    maskedComponent = maskedComponent.replaceAll(
+      this.getDelimiterCharacter(),
+      `${ESCAPE_CHARACTER}${this.getDelimiterCharacter()}`
+    );
+
+    return maskedComponent;
+  }
+
+  public isEmpty(): boolean {
+    return this.getNoComponents() === 0;
+  }
+
+  public getComponent(x: number): string {
+    if (!this.isValidIndexForComponentsArray(x)) {
+      console.error(`${x} is not a valid index for the components array!`);
+      return "";
     }
 
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+    const components: string[] = this.getComponents();
+    return components[x];
+  }
+
+  public setComponent(n: number, c: string): void {
+    if (!this.isValidIndexForComponentsArray(n)) {
+      console.error(`${n} is not a valid index for the components array!`);
+      return;
     }
 
-    public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+    const components: string[] = this.getComponents();
+    components[n] = c;
+    this.setName(components.join(this.getDelimiterCharacter()));
+  }
+
+  public insert(n: number, c: string): void {
+    const components: string[] = this.getComponents();
+    const isValidInsertIndex: boolean =
+      Number.isInteger(n) && n >= 0 && n <= components.length;
+    if (!isValidInsertIndex) {
+      console.error(
+        `${n} is not a valid *insertion* index for the components array!`
+      );
+      return;
     }
 
-    public getComponent(x: number): string {
-        throw new Error("needs implementation or deletion");
+    components.splice(n, 0, c);
+    this.setName(components.join(this.getDelimiterCharacter()));
+    this.setNoComponents(components.length);
+  }
+
+  public append(c: string): void {
+    const components: string[] = this.getComponents();
+    this.setNoComponents(components.push(c));
+    this.setName(components.join(this.getDelimiterCharacter()));
+  }
+
+  public remove(n: number): void {
+    if (!this.isValidIndexForComponentsArray(n)) {
+      console.error(`${n} is not a valid index for the components array!`);
+      return;
     }
 
-    public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
-    }
+    const components: string[] = this.getComponents();
+    components.splice(n, 1);
+    this.setName(components.join(this.delimiter));
+    this.setNoComponents(components.length);
+  }
 
-    public insert(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
-    }
+  public concat(other: Name): void {
+    const delimiter: string = this.getDelimiterCharacter();
+    this.setName(this.asString() + delimiter + other.asString());
 
-    public append(c: string): void {
-        throw new Error("needs implementation or deletion");
-    }
+    const components: string[] = this.getComponents();
+    this.setNoComponents(components.length);
+  }
 
-    public remove(n: number): void {
-        throw new Error("needs implementation or deletion");
-    }
+  private isValidIndexForComponentsArray(i: number): boolean {
+    const components: string[] = this.getComponents();
+    const isInteger: boolean = Number.isInteger(i);
+    const isInBounds: boolean = i >= 0 && i < components.length;
 
-    public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
-    }
-
+    return isInteger && isInBounds;
+  }
 }
